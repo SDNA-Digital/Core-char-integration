afab14297f406200a2618e8f8ee0a9a2b475cc21
Vini347 <contato.viniciusdasilva@gmail.com>
2023-05-02 11:12:18 -0300

    Nova branch

---
+++ dataJson/dataJson/views.py
@@ -0,0 +1,30 @@
+from django.http import JsonResponse
+import json
+import psycopg2 as pg
+
+
+def dataJson(request):
+
+    x = []
+    y = []
+    label=[]
+    connection = pg.connect(user="postgres", password="SDNA@2022", host="localhost", port="5432", database="Eagle2")
+    curs = connection.cursor()
+    curs.execute('SELECT areaid as "AreaID", areanome as "Area", COUNT(*) as "QtdeIncidentes" FROM dash_incidentes GROUP BY areaid, areanome')
+
+    columns = [col[0] for col in curs.description]
+    results = []
+    for row in curs.fetchall():
+        result_dict = {}
+        for i, value in enumerate(row):
+            result_dict[columns[i]] = value
+        results.append(result_dict)
+
+    curs.close
+    connection.close
+
+    json_result = json.dumps(results, indent=4)
+
+    with open('resultado.json', 'w') as arquivo:
+        arquivo.write(json_result)
+        return JsonResponse(results, safe=False)
